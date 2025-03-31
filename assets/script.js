2a6e6bfa1faca942e10b79e73b08d97d350a95a5
edvyn <55361887+edvyn@users.noreply.github.com>
2025-03-31 12:40:34 -0300

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -18,14 +18,35 @@ hamburguer.addEventListener('click', () => {
 
 document.addEventListener("DOMContentLoaded", function () {
     const checkbox = document.getElementById("checkboxInput");
-    const body = document.body;
+    const particlesDiv = document.getElementById("particles-js");
+    const navcontainerDiv = document.getElementById('navcontainer');
 
     checkbox.addEventListener("change", function () {
         if (this.checked) {
-            body.style.backgroundColor = "#3EA6B1"; // Cor quando ativado
+            particlesDiv.style.backgroundColor = "#15383C"; // Cor quando ativado
         } else {
-            body.style.backgroundColor = "#171818";// Cor quando desativado
+            particlesDiv.style.backgroundColor ='#6da0a3'; // Cor quando desativado
         }
     });
 });
 
+particlesJS("particles-js", {
+    "particles": {
+      "number": { "value": 40, "density": { "enable": true, "value_area": 600 } },
+      "color": { "value": "#4FC3F7" },
+      "shape": { "type": "circle" },
+      "opacity": { "value": 0.7, "random": false },
+      "size": { "value": 4, "random": true },
+      "move": { "speed": 1, "direction": "none", "random": false }
+    },
+    "interactivity": {
+      "events": {
+        "onhover": { "enable": true, "mode": "repulse" },
+        "onclick": { "enable": true, "mode": "push" } 
+      },
+      "modes": {
+        "repulse": { "distance": 1000, "duration": 0.4 },
+        "push": { "particles_nb": 4 } 
+      }
+    }
+  });
