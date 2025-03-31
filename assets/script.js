230b91184fef9985ddde37063ee6c738ac30c23e
edvyn <55361887+edvyn@users.noreply.github.com>
2025-03-31 16:15:53 -0300

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -19,16 +19,42 @@ hamburguer.addEventListener('click', () => {
 document.addEventListener("DOMContentLoaded", function () {
     const checkbox = document.getElementById("checkboxInput");
     const particlesDiv = document.getElementById("particles-js");
-    const navcontainerDiv = document.getElementById('navcontainer');
 
-    checkbox.addEventListener("change", function () {
-        if (this.checked) {
+    // Verifica se há um valor salvo no localStorage
+    const savedState = localStorage.getItem("checkboxState");
+    if (savedState === "true") {
+        checkbox.checked = true;
+    } else {
+        checkbox.checked = false;
+    }
+
+    function updateStyles() {
+        if (checkbox.checked) {
             particlesDiv.style.backgroundColor = "#15383C"; // Cor quando ativado
+            document.documentElement.style.setProperty("--primary-color", "#C1F9FF");
+            document.documentElement.style.setProperty("--tertiary-color", "#C1F9FF");
+            document.documentElement.style.setProperty("--aux-color", "#3EA6B1");
+            document.documentElement.style.setProperty("--dropdown-color", "#171818a8;");
+
+            localStorage.setItem("checkboxState", "true"); // Salva o estado ativado
         } else {
-            particlesDiv.style.backgroundColor ='#6da0a3'; // Cor quando desativado
+            particlesDiv.style.backgroundColor = "#6da0a3"; // Cor quando desativado
+            document.documentElement.style.setProperty("--primary-color", "#15383C");
+            document.documentElement.style.setProperty("--tertiary-color", "#15383C");
+            document.documentElement.style.setProperty("--aux-color", "#15383C");
+            document.documentElement.style.setProperty("--dropdown-color", "#171818a8;");
+            localStorage.setItem("checkboxState", "false"); // Salva o estado desativado
         }
-    });
+    }
+
+    // Aplica o estilo ao carregar a página
+    updateStyles();
+
+    // Escuta mudanças no checkbox e atualiza o estado
+    checkbox.addEventListener("change", updateStyles);
 });
+
+
 
 particlesJS("particles-js", {
     "particles": {
